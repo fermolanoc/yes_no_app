18e7e4401e535bd5e3b811c2707cd542c4c5b6cd
Fernando Molano <53359507+fermolanoc@users.noreply.github.com>
2024-09-13 13:17:59 -0400

    Initial project structure

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Yes - No App'),
+          ),
+          body: Center(
+            child: FilledButton.tonal(
+                onPressed: () {}, child: const Text('Click me')),
+          )),
+    );
+  }
+}
